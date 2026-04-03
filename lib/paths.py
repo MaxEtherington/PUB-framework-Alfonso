a06c2c15d00d9ffb879cea2a50b3687927bef2cc
Maxwell Etherington <max.etho@gmail.com>
2026-04-03 11:40:53 +1100

    Add filesystem initialisation

--- lib/paths.py
+++ lib/paths.py
@@ -21,4 +21,9 @@ PLATE_MODEL_DIR = EXTRACTED_DATA_DIR / 'plate_models' / config['plate_model']['p
 RASTER_DATA_DIR = EXTRACTED_DATA_DIR / 'rasters'
 POINTS_DATA_DIR = EXTRACTED_DATA_DIR / 'polygons_points' / f"{config['reference_feature']}_{config['study_zone_buffer']}_deg_buffer"
 
-OUTPUT_DIR = ROOT / config['output_dir'] / config['run_name']
+OUTPUT_DIR = ROOT / 'output' / config['run_name']
+
+# =============== Initialise directories ===============
+
+for path in [EXTRACTED_DATA_DIR, PLATE_MODEL_DIR, RASTER_DATA_DIR, POINTS_DATA_DIR, OUTPUT_DIR]:
+    path.mkdir(parents=True, exist_ok=True)
