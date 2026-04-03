a7227effdc4e1076dc2863c79b807eb1987f22a3
Maxwell Etherington <max.etho@gmail.com>
2026-04-03 11:33:24 +1100

    Create paths.py to centralise filepath creation/management

---
+++ lib/paths.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from load_params import get_params
+
+# =============== Load config =============== 
+
+ROOT = Path(__file__).resolve().parent.parent
+CONFIG_PATH = ROOT / 'config' / '.run_config.yml'
+config = get_params(CONFIG_PATH)
+
+# =============== Data paths ===============
+
+PREPARED_DATA_DIR = ROOT / 'data_prepared'
+
+SOURCE_DATA_DIR = ROOT / 'data_source'
+MANTLE_DATA_DIR = SOURCE_DATA_DIR / 'mantle_outputs' / config['mantle_features']['mantle_dir']
+DEPOSITS_PATH = SOURCE_DATA_DIR / "deposits" / config['deposits_filename']
+REGIONS_PATH = SOURCE_DATA_DIR / "regions" / config['regions_filename']
+
+EXTRACTED_DATA_DIR = ROOT / 'data_extracted' / config['plate_model']['plate_model_name']
+PLATE_MODEL_DIR = EXTRACTED_DATA_DIR / 'plate_models' / config['plate_model']['plate_model_name']
+RASTER_DATA_DIR = EXTRACTED_DATA_DIR / 'rasters'
+POINTS_DATA_DIR = EXTRACTED_DATA_DIR / 'polygons_points' / f"{config['reference_feature']}_{config['study_zone_buffer']}_deg_buffer"
+
+OUTPUT_DIR = ROOT / config['output_dir'] / config['run_name']
